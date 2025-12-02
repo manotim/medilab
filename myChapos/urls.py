d8fd686d13fa3ed4f17ca7fe2f36c8e74cc416fd
Mike Manoti <manotimike@gmail.com>
2025-12-02 19:38:34 +0300

    done v1

--- myChapos/urls.py
+++ myChapos/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("index/", views.index, name="index"),
     path("starter/", views.starter, name="starter-page"),
     path("about/", views.about, name="about"),
     path("services/", views.services, name="services"),
-    path("appointment/", views.appoint, name="appointment")
+    path("appointment/", views.appoint, name="appointment"),
+    path("show/", views.show, name="show"),
+    path('show/delete/<int:id>', views.delete),
+    path('show/edit/<int:id>', views.edit),
+    path('', views.register, name='register'),
+    path('login/', views.login_user, name='login')
 ]
